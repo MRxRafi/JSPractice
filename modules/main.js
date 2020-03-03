ce8dbcfc0f332c97e7595ba1999535c33e212343
MRxRafi <rafadriveclase@gmail.com>
2020-03-03 20:28:09 +0100

    [JS MIX] Modules, webpack and typescript (one exercise is pending)

---
+++ modules/main.js
@@ -0,0 +1,23 @@
+import addCounter from "./module.js";
+
+// Elements
+const [input, button, counters] = document.querySelectorAll('input, button, #counters');
+
+// Events
+input.onkeyup = (e) => {
+    const seconds = e.target.value;
+
+    button.textContent = seconds ? `Contar ${seconds} segundos` : 'Contar';
+
+    if (e.key === 'Enter') {
+        button.click();
+    }
+}
+
+button.onclick = () => {
+    if (input.value) {
+        addCounter(input.value, counters);
+    }
+    input.focus();
+    input.select();
+}
